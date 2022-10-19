fbd66f6b6e0c243cf546fa207d098eb50a19d708
sahanab2000 <85185990+sahanab2000@users.noreply.github.com>
2022-10-19 20:25:12 +0530

    Add files via upload

---
+++ day 15 UC2.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HashTableExample
+{
+    public class HyMapNode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = n Represents a 32 - bit; signed integer.>[size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public v Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<k, V>> LinkedList GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default();
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<k, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                LinkedList.Remove(foundItem);
+            }
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> LinkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+    }
+    public struct KeyValue<k, v>
+    {
+        public k Key { get; set; }
+        public v Value { get; set; }
+    }
+}
